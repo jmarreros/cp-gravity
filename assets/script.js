8a31c0d3d8af238e34abc8b6d8c9a1656af987a0
Jhon Marreros Guzmán <j_marreros@hotmail.com>
2024-05-21 15:21:13 -0500

    Mejoras en accesibilidad seleccion ciudades

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,54 @@
-(function( $ ) {
+(function ($) {
     'use strict';
 
-    const data = {};
+    generic_fill_fields('.grupo1');
+    generic_fill_fields('.grupo2');
 
-    $('.datos.cp .ginput_container_text').click(function(e){
+    function generic_fill_fields(grupo_sel) {
+        let data = {};
 
-        if (e.offsetX > $(this).width() - 70 ) {
-            const code = $(this).find('input').val();
 
+        const cpContainer = $(grupo_sel + '.cp .ginput_container_text');
+        const cpDesc = $(grupo_sel + '.cp .gfield_description');
+
+        const cities = $(grupo_sel + '.cities select'); // Dynamic select
+
+        const ciudad = $(grupo_sel + '.ciudad input');
+        const municipio = $(grupo_sel + '.municipio input');
+        const provincia = $(grupo_sel + '.provincia input');
+        const comunidad = $(grupo_sel + '.comunidad input');
+
+
+        $(cities).change(function () {
+            if (data) {
+                const current = $(this).val();
+
+                // Search current in data
+                const result = data.filter(item => item.ciudad === current);
+
+                if (result.length > 0) {
+                    $(ciudad).val(result[0].ciudad);
+                    $(municipio).val(result[0].municipio);
+                    $(provincia).val(result[0].provincia);
+                    $(comunidad).val(result[0].ca);
+                }
+            }
+        });
+
+
+        $(cpContainer).find('input').on('focusout', function () {
+            ajax_call();
+        });
+
+        $(cpContainer).click(function (e) {
+            if (e.offsetX > $(this).width() - 70) {
+                ajax_call();
+            }
+        });
+
+
+        function ajax_call(){
+            const code = $(cpContainer).find('input').val();
             $.ajax({
                 url: cpgravity_var.ajaxurl,
                 type: 'post',
@@ -17,18 +58,41 @@
                     nonce: cpgravity_var.nonce,
                     code: code
                 }, beforeSend: function () {
-                    // Clean inputs
+                    $(cpDesc).show();
+                    $(cities).find('option').remove().end();
+                    $(ciudad).val('');
+                    $(municipio).val('');
+                    $(provincia).val('');
+                    $(comunidad).val('');
                 }
             })
-            .done(function (res) {
-                console.log(res);
-            })
-            .always(function () {
-                // Enable elements
-            });
+                .done(function (res) {
+                    if (res.success) {
+                        if (res.data.length > 0) {
 
+                            if ( res.data.length > 1){
+                                $(cities).append('<option value="">Selecciona una ciudad</option>');
+                            }
+
+                            res.data.forEach(item => {
+                                $(cities).append('<option value="' + item.ciudad + '">' + item.ciudad + '</option>');
+                            });
+
+                            data = res.data;
+
+                            if ( res.data.length === 1){
+                                $(cities).change();
+                            }
+
+                        } else {
+                            alert('No se han encontrado resultados');
+                        }
+                    }
+                })
+                .always(function () {
+                    $(cpDesc).hide();
+                });
         }
+    }
 
-    });
-
-})( jQuery );
+})(jQuery);
