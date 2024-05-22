c1901923b6cbdf39fa50391a1afc0fb742a3d48d
Jhon Marreros Guzmán <j_marreros@hotmail.com>
2024-05-22 16:12:22 -0500

    Agregado información pantalla documentación

--- views/main-screen.php
+++ views/main-screen.php
@@ -1,3 +1,15 @@
 <div class="wrap">
 
-<h1>Código Postal Integración Gravity</h1>
+    <h1>Código Postal Integración Gravity</h1>
+    <p>
+        Integración de código postal en Gravity Forms para llenar automáticamente campos de ciudad, municipio, provincia y comunidad autónoma.
+    </p>
+    <p>
+        Se requiere las siguientes tablas en la BD:
+    </p>
+
+    <ul>
+        <li>data_ccaa_provincias</li>
+        <li>data_cp</li>
+        <li>data_municipios</li>
+    </ul>
